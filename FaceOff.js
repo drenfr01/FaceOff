4771798d4b4b4b45bd61497792227268ccf28880
Tyler Sheffels <Tyler.Sheffels@gmail.com>
2013-11-03 15:38:59 -0500

    Adding states and in_game page

--- FaceOff.js
+++ FaceOff.js
@@ -2,18 +2,28 @@ Users = new Meteor.Collection("users");
 Cards = new Meteor.Collection("cards");
 
 if (Meteor.isClient) {
+  Meteor.startup(function () {
+    Session.set("state", "landing")
+  })
 
   Template.main.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+    'click #new_game' : function () {
+      Session.set("state", "in_game");
     }
   });
+
+  Template.main.getState = function () {
+    return Session.get("state");
+  }
+
+  Template.main.stateIs = function (state) {
+    return Session.get("state") === state
+  }
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
+    //Set the initial State of the application
     Cards.remove({});
     if (Cards.find().count() == 0) {
       }
