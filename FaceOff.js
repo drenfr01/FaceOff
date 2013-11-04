9fd5a2d1bff07cf92ae6ac6e347f81d297625c8d
Tyler Sheffels <Tyler.Sheffels@gmail.com>
2013-11-03 19:49:41 -0500

    Update image list

--- FaceOff.js
+++ FaceOff.js
@@ -78,7 +78,12 @@ if (Meteor.isServer) {
                         ,'0002.jpg'
                         ,'0003.jpg'
                         ,'0004.jpg'
-                        ];
+                        ,'0005.jpg'
+                        ,'0006.jpg'
+                        ,'0007.jpg'
+                        ,'0008.jpg'
+                        ,'0009.jpg'
+                        ,'0000.jpg'];
     //path: relative path to file, active: still in the game, in_play: currently on the board
     for (var i = 0; i < image_paths.length; i++)
       Cards.insert({path: image_paths[i], active: 1, in_play: 0});
