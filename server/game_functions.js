21f60e5af9f321169afca538c42cd7a24c88f9d3
drenfr01 <duncanrenfrow@gmail.com>
2013-12-26 22:23:47 -0500

    adding in semi-colons to appease style guide

--- server/game_functions.js
+++ server/game_functions.js
@@ -2,21 +2,21 @@ getNextImages = function (gameNumber) {
   //Clear cards currently in play
   Cards.find({active: gameNumber}).forEach( function (card) {
     Cards.update( {_id: card._id}, {$pull: { in_play: gameNumber } } );
-  })
+  });
 
   //Put new cards into play
   selectRandomCards(2).forEach(function (id) {
     Cards.update( {_id: id}, {$push: { in_play: gameNumber} } , {$set: {votes: 0} } );
-  })
-  setTimer(gameNumber, getVotingTime(gameNumber), "endVoting")
-}
+  });
+  setTimer(gameNumber, getVotingTime(gameNumber), "endVoting");
+};
 
 endVoting = function(gameNumber) {
   // TODO: Update the votes to be visible
 
   // Start the next round in 7 seconds
-  setTimer(7, "getNextImages")
-}
+  setTimer(7, "getNextImages");
+};
 
 
 selectRandomCards = function(gameNumber, numberOfCards) {
@@ -27,13 +27,13 @@ selectRandomCards = function(gameNumber, numberOfCards) {
   //insert each path into the cards array
   cards.forEach( function (card) {
     cards_array.push( card._id );
-  })
+  });
 
-  var card1 = Random.choice(cards_array)
+  var card1 = Random.choice(cards_array);
   var filtered_cards_array = cards_array.filter( function (element) {
-    return element != card1
+    return element != card1;
   });
   var card2 = Random.choice(filtered_cards_array);
 
   return [card1, card2];
-}
+};
