40c9eb9a5f442fea5ac9b9da5e331a9ee7ef57ba
drenfr01 <duncanrenfrow@gmail.com>
2014-01-22 20:32:25 -0500

    fixing bug with meteor calls

--- client/views/game.js
+++ client/views/game.js
@@ -1,6 +1,16 @@
 Template.game.events({
   'click' : function () {
     //need a call to server side message to update users
-    Router.go('inGame', {number: this.number});
+    //TODO: add hook to make sure user logged in
+
+    Meteor.call('joinUserToGame', Meteor.userId, this.number, 
+      function(error, result) {
+        if(error) {
+          alert(error.reason);
+        } 
+        
+        Router.go('inGame', {number: result});
+      }
+    );
   }
 });
