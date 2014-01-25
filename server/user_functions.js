0da6509d8c1b5968329d0330d90c2b65ffd97579
drenfr01 <duncanrenfrow@gmail.com>
2014-01-25 17:00:01 -0500

    Can no longer vote during display phase

--- server/user_functions.js
+++ server/user_functions.js
@@ -8,11 +8,18 @@ Meteor.methods({
   },
   voteForImage: function(userId, imageId) {
       user = Meteor.users.findOne({_id: userId});
+      usersGame = Games.findOne({number: user.gameNumber});
+      if(usersGame.phase === "Display") {
+        throw new Meteor.Error(403, 
+          "Can't vote while displaying results of voting...",
+          user._id);
+      }
       if(user.hasVoted) {
         throw new Meteor.Error(302, 
           "You've already voted!!!",
           user._id);
-      }
+      } 
+      
       Meteor.users.update(userId, {$set: {hasVoted: 1}}); 
       //TODO: this assumes card is only in one game
       Cards.update(imageId, {$push: {usersVoting: userId}});
