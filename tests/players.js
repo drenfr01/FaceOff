33d561867cc2a3b4ecaf0ce697690c3a17201827
drenfr01 <duncanrenfrow@gmail.com>
2014-02-23 17:09:47 -0500

    Finishing unit tests for existing players database access functions

--- tests/players.js
+++ tests/players.js
@@ -55,4 +55,91 @@ suite('Players', function() {
       done();
     });
   });
+
+  test('using the getNextPlayerCard function', function(done, server) {
+    server.eval(function() {
+      //setup
+      gameNumber = 0;
+      cardId1 = addCard('http://image1.com',gameNumber);
+      cardId2 = addCard('http://image2.com',gameNumber);
+      playerId = addPlayer(gameNumber, 'test@gmail.com');
+
+      addCardToPlayer(playerId, cardId1);
+      addCardToPlayer(playerId, cardId2);
+
+      playerBefore = Players.findOne(playerId);
+
+      //nextCard should be first card added to Player
+      nextCard = getNextPlayerCard(playerId);
+
+      playerAfter = Players.findOne(playerId);
+
+      var options = {
+        cardId1: cardId1,
+        cardId2: cardId2,
+        nextCard: nextCard,
+        playerBefore: playerBefore,
+        playerAfter: playerAfter
+      };
+      emit('options',options);
+    });
+
+    server.once('options', function(options) {
+      assert.equal(options.cardId1,options.nextCard);
+      
+      assert.deepEqual(options.playerBefore.cardIds,
+        [options.cardId1,options.cardId2]);
+
+      assert.deepEqual(options.playerAfter.cardIds,
+        [options.cardId2,options.cardId1]);
+      done();
+    });
+  });
+  test('testing the setPlayerVote function', function(done, server) {
+    server.eval(function() {
+      gameNumber = 0;
+      playerId = addPlayer(gameNumber, 'test@gmail.com');
+
+      playerBefore = Players.findOne(playerId);
+      setPlayerVote(playerId);
+      playerAfter = Players.findOne(playerId);
+      
+      var options = {
+        playerBefore: playerBefore,
+        playerAfter: playerAfter
+      };
+      emit('options', options);
+
+    });
+
+    server.once('options', function(options) {
+      assert.equal(options.playerBefore.hasVoted, 0);
+      assert.equal(options.playerAfter.hasVoted, 1);
+      done();
+    });
+  });
+  test('testing the removePlayerVote function', function(done, server) {
+    server.eval(function() {
+      gameNumber = 0;
+      playerId = addPlayer(gameNumber, 'test@gmail.com');
+
+      setPlayerVote(playerId);
+      playerBefore = Players.findOne(playerId);
+      removePlayerVote(playerId);
+      playerAfter = Players.findOne(playerId);
+      
+      var options = {
+        playerBefore: playerBefore,
+        playerAfter: playerAfter
+      };
+      emit('options', options);
+
+    });
+
+    server.once('options', function(options) {
+      assert.equal(options.playerBefore.hasVoted, 1);
+      assert.equal(options.playerAfter.hasVoted, 0);
+      done();
+    });
+  });
 });
